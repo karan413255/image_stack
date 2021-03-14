c269b60542b857563dd10d5dbbf6d22a18254f18
Karan Shah <karanshah413255@gmail.com>
2021-03-14 14:30:05 +0530

    Package Documentation (#5)
    * Providing documentation to the package

--- lib/image_stack.dart
+++ lib/image_stack.dart
@@ -3,25 +3,69 @@ library image_stack;
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-/// Creates an image stack
+/// Creates an array of circular images stacked over each other
 class ImageStack extends StatelessWidget {
+  /// List of image urls
   final List<String> imageList;
+
+  /// Image radius for the circular image
   final double? imageRadius;
+
+  /// Count of the number of images to be shown
   final int? imageCount;
+
+  /// Total count will be used to determine the number of circular images
+  /// to be shown along with showing the remaining count in an additional
+  /// circle
   final int totalCount;
+
+  /// Optional field to set the circular image border width
   final double? imageBorderWidth;
+
+  /// Optional field to set the color of circular image border
   final Color? imageBorderColor;
+
+  /// The text style to apply if there is any extra count to be shown
   final TextStyle extraCountTextStyle;
+
+  /// Set the background color of the circle
   final Color backgroundColor;
+
+  /// Enum to define the image source.
+  /// 
+  /// Describes type of the image source being sent in [imageList]
+  /// 
+  /// Possible values:
+  ///  * Asset
+  ///  * Network
+  ///  * File
   final ImageSource? imageSource;
+
+  /// Custom widget list passed to render circular images
   final List<Widget> children;
+
+  /// Radius for the circular image to applied when [children] is passed
   final double? widgetRadius;
+
+  /// Count of the number of widget to be shown as circular images when [children]
+  /// is passed
   final int? widgetCount;
+
+  /// Optional field to set the circular border width when [children] is passed
   final double? widgetBorderWidth;
+
+  /// Optional field to set the color of circular border when [children] is passed
   final Color? widgetBorderColor;
+
+  /// List of `ImageProvider`
   final List<ImageProvider> providers;
+
+  /// To show the remaining count if the provided list size is less than [totalCount]
   final bool showTotalCount;
 
+  /// Creates a image stack widget.
+  ///
+  /// The [imageList] and [totalCount] parameters are required.
   ImageStack({
     Key? key,
     required this.imageList,
@@ -45,6 +89,9 @@ class ImageStack extends StatelessWidget {
         widgetRadius = null,
         super(key: key);
 
+  /// Creates a image stack widget by passing list of custom widgets.
+  ///
+  /// The [children] and [totalCount] parameters are required.
   ImageStack.widgets({
     Key? key,
     required this.children,
@@ -68,6 +115,9 @@ class ImageStack extends StatelessWidget {
         imageSource = null,
         super(key: key);
 
+  /// Creates an image stack by passing list of `ImageProvider`.
+  /// 
+  /// The [providers] and [totalCount] parameters are required.
   ImageStack.providers({
     Key? key,
     required this.providers,
@@ -158,44 +208,45 @@ class ImageStack extends StatelessWidget {
     }
     int _renderedImageSize = images.length > 0
         ? images.length
-        : children.length > 0 ? children.length : providersImages.length;
+        : children.length > 0
+            ? children.length
+            : providersImages.length;
     return Container(
       child: Row(
         children: <Widget>[
           images.isNotEmpty || widgets.isNotEmpty || providersImages.isNotEmpty
               ? Stack(
-                  clipBehavior: Clip.none, 
+                  clipBehavior: Clip.none,
                   textDirection: TextDirection.rtl,
                   children: children.length > 0
                       ? widgets
-                      : providers.length > 0 ? providersImages : images,
+                      : providers.length > 0
+                          ? providersImages
+                          : images,
                 )
               : SizedBox(),
           Container(
-            margin: EdgeInsets.only(left: 5),
-            child: totalCount - _renderedImageSize > 0
-                ? showTotalCount
-                    ? Container(
-                        constraints: BoxConstraints(minWidth: imageRadius!),
-                        padding: EdgeInsets.all(3),
-                        height: imageRadius,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(imageRadius!),
-                            border: Border.all(
-                                color: imageBorderColor!,
-                                width: imageBorderWidth!),
-                            color: backgroundColor),
-                        child: Center(
-                          child: Text(
-                            (totalCount - images.length).toString(),
-                            textAlign: TextAlign.center,
-                            style: extraCountTextStyle,
-                          ),
+              margin: EdgeInsets.only(left: 5),
+              child: showTotalCount && totalCount - _renderedImageSize > 0
+                  ? Container(
+                      constraints: BoxConstraints(minWidth: imageRadius!),
+                      padding: EdgeInsets.all(3),
+                      height: imageRadius,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(imageRadius!),
+                          border: Border.all(
+                              color: imageBorderColor!,
+                              width: imageBorderWidth!),
+                          color: backgroundColor),
+                      child: Center(
+                        child: Text(
+                          (totalCount - images.length).toString(),
+                          textAlign: TextAlign.center,
+                          style: extraCountTextStyle,
                         ),
-                      )
-                    : SizedBox()
-                : Container(),
-          ),
+                      ),
+                    )
+                  : SizedBox()),
         ],
       ),
     );
